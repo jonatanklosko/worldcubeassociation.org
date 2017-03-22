252178c47a093e06020b60c66bfd4f6a534393d3
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-03-22 16:18:47 +0100

    Add some links to serializers. Add a status attribute to registration serializer.

--- WcaOnRails/app/serializers/registration_serializer.rb
+++ WcaOnRails/app/serializers/registration_serializer.rb
@@ -1,9 +1,14 @@
 class RegistrationSerializer < ActiveModel::Serializer
-  attributes :id, :accepted_at, :deleted_at
+  attributes :id
+  attribute :status do
+    object.checked_status
+  end
 
   has_one :user
   has_one :competition
   has_many :events
 
   link(:self) { api_v1_competition_registration_url object.competition, object }
+  link(:user) { api_v1_user_url object.user }
+  link(:competition) { api_v1_competition_url object.competition }
 end
